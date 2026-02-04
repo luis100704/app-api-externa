81c628dc8f5370290f84436ead359af271d5c55d
Luis Fernando Eugenio Castillo <luiseugenio808@gmail.com>
2026-02-04 21:09:17 +0100

    consumo inicial de la API de películas

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
-export default function HomePage() {
+type Movie = {
+  id: number;
+  title: string;
+};
+
+async function getPopularMovies(): Promise<Movie[]> {
+  const apiKey = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+
+  const response = await fetch(
+    `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=es-ES`
+  );
+
+  const data = await response.json();
+  return data.results;
+}
+
+export default async function HomePage() {
+  const movies = await getPopularMovies();
+
   return (
     <main>
-      <h1>Aplicación de Películas</h1>
-      <p>Proyecto frontend con Next.js</p>
+      <h1>Películas populares</h1>
+
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
     </main>
-  );
+  )
 }
