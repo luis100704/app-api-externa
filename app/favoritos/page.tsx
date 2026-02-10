ef2b7f7c0a49dd57d46fbd048c9147a0ba274fe2
Luis Fernando Eugenio Castillo <luiseugenio808@gmail.com>
2026-02-10 19:39:03 +0100

    crear página base de favoritos

---
+++ app/favoritos/page.tsx
@@ -0,0 +1,7 @@
+export default function FavoritosPage() {
+    return (
+        <main className="p-4">
+            <h1 className="text-xl font-bold">Mis películas favoritas</h1>
+        </main>
+    );
+}
